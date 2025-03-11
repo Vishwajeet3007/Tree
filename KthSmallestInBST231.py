a491cc33ddb8834576caa0ee262d1ec6f88345bc
Vishwajeet Kumar <vishwa843113@gmail.com>
2025-03-11 09:08:44 +0530

    add

---
+++ KthSmallestInBST231.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self,val=None,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution:
+    def kthSmallest(self,root,k):
+        self.count = 0
+        self.ans = None
+        self.helper(root,k)
+        return self.ans
+    
+    def helper(self,root,k):
+        if not root:
+            return
+        
+        self.helper(root.left,k)
+        self.count += 1
+        if self.count == k:
+            self.ans = root.val
+            return
+        
+        self.helper(root.right,k)
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(7)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+root.right.right = TreeNode(8)
+
+sol = Solution()
+print(sol.kthSmallest(root, 3))  # Find the 3rd smallest element
+
+
+        
